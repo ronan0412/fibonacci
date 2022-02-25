645514a785ce89fa0a34c88944d8ff396ecff7a8
Kosuke <darthbenirece@gmail.com>
2022-02-25 19:19:42 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 'use strict';
+const memo = new Map();
+memo.set(0, 0);//キー左が0で、値右が0
+memo.set(1, 1);
+
 function fib(n) {
-    if (n === 0) {
-        return 0;
-    } else if (n === 1) {
-        return 1;
-    }
-    return fib(n - 1) + fib(n - 2);
+    if (memo.has(n)) {//Mapがnをキーとした値を持ってたら
+        return memo.get(n);//nに対応した値を返す
+    } 
+    //Mapがnをキーとした値を持ってなかったら、
+    //再帰関数を計算して値を求め、
+    const value = fib(n - 1) + fib(n - 2);
+    memo.set(n, value);//それをMapに格納して
+    return value;//返す
 }
+
 const length = 40;
-for (let i = 0; i <= length; i ++) {
+for (let i = 0; i <= length; i ++) {    
     console.log(fib(i));
 }
